c5338b7ed10b3e42f5d9fc06740100c4e46f047e
jiangming <jiangming@xiaomi.com>
2013-08-24 19:16:49 +0800

    bb
    Signed-off-by: jiangming <jiangming@xiaomi.com>

--- src/backend.go
+++ src/backend.go
@@ -4,6 +4,7 @@ import(
 	"net/http"
 	"html/template"
 	"fmt"
+	"article"
 )
 
 func initBackendHandler(){
@@ -28,8 +29,11 @@ func backendManagerLogout(w http.ResponseWriter, r *http.Request){
 // article create
 func backendArticleCreate(w http.ResponseWriter, r *http.Request){
 	//fmt.Fprintf(w, "article create")
-	t, err := template.New("create").Parse(`{{define "T"}}Hello, {{.}}{{end}}`)
-	err = t.ExecuteTemplate(w, "T", template.HTML("<script>alert('Test')</script>"))
+	fmt.Println("method:", r.Method)
+	article := article.Article{Id:100, Title:"Yiishare"}
+	t, err := template.ParseFiles("templates/default/article_create.html")
+	//err = t.ExecuteTemplate(w, "T", template.HTML("<script>alert('Test')</script>"))
+	err = t.ExecuteTemplate(w, "create", article)
 	if err != nil{
 		fmt.Println(err.Error())
 	}
